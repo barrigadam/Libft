38bc76ed578ce2c8bd2edecc6651793768a93756
Alberto Barriga Mato <abarriga@c2r7s6.42malaga.com>
2022-10-04 17:36:58 +0200

    04/10

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abarriga <abarriga@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 16:26:39 by abarriga          #+#    #+#             */
-/*   Updated: 2022/09/30 17:58:26 by abarriga         ###   ########.fr       */
+/*   Updated: 2022/10/03 20:08:37 by abarriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"libft.h"
@@ -21,12 +21,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	node1 = 0;
 	while (lst)
 	{
-		a = ft_lstnew(f(lst->content));
+		a = ft_lstnew(0);
 		if (!a)
 		{
 			ft_lstclear(&node1, del);
 			return (0);
 		}
+		a->content = f(lst->content);
 		ft_lstadd_back(&node1, a);
 		lst = lst->next;
 	}
